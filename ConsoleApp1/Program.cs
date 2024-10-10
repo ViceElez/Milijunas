b5ad882d273b9eab78772b271635d81720dbb98a
ViceElez <vice.elezovic12@gmail.com>
2024-10-10 11:55:04 +0200

    promjena u consoleapp1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,18 +1,5 @@
-﻿Console.WriteLine("Upisite rijec");
-string rijec=Console.ReadLine();
-string rijecSMalimSlovima = rijec.ToLower();
-int j = rijecSMalimSlovima.Length-1;
-int br = 0;
-for (int i = 0; i < rijecSMalimSlovima.Length; i++)
-{
-    if (rijecSMalimSlovima == " ")
-        i--;
-    else
-    {
-        br++;
-    }
-}
-Console.WriteLine(br);
+﻿Console.WriteLine("Hello world!");
+
 
 //broj rijeci u recenici i broj slova u recenici
 
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7f8d2a23000fea409183918e0b94a1683d85e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
